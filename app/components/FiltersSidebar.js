7bc59e880188e617028248c3e37656d2c123607a
anaitullah31 <anaitullah198@gmail.com>
2026-04-30 18:40:12 +0800

    Load category on sidebar

--- app/components/FiltersSidebar.js
+++ app/components/FiltersSidebar.js
@@ -1,34 +1,42 @@
-export default function FiltersSidebar() {
+import { loadAllBooks } from "../lib/utils";
+
+export default async function FiltersSidebar() {
+  const books = await loadAllBooks();
+  const categories = [...new Set(books.map((book) => book.category))];
   return (
     <aside className="w-64 shrink-0 hidden lg:flex flex-col gap-8">
       <div>
-        <h3 className="text-sm font-bold text-[#170C79] uppercase mb-3">
+        <h3 className="text-sm font-bold text-[#000000] uppercase mb-3">
           Genre
         </h3>
 
-        {["Fiction", "Philosophy", "History", "Science"].map((item) => (
+        {categories.map((item) => (
           <label key={item} className="flex items-center gap-3 mb-2">
-            <input type="checkbox" className="accent-[#170C79]" />
+            <input type="checkbox" className="accent-[#000000]" />
             <span className="text-gray-600">{item}</span>
           </label>
         ))}
       </div>
 
       <div>
-        <h3 className="text-sm font-bold text-[#170C79] uppercase mb-3">
+        <h3 className="text-sm font-bold text-[#000000] uppercase mb-3">
           Availability
         </h3>
 
         {["All Titles", "Available Now"].map((item) => (
           <label key={item} className="flex items-center gap-3 mb-2">
-            <input type="radio" name="availability" className="accent-[#170C79]" />
+            <input
+              type="radio"
+              name="availability"
+              className="accent-[#170C79]"
+            />
             <span className="text-gray-600">{item}</span>
           </label>
         ))}
       </div>
 
       <div>
-        <h3 className="text-sm font-bold text-[#170C79] uppercase mb-3">
+        <h3 className="text-sm font-bold text-[#000000] uppercase mb-3">
           Publication Year
         </h3>
 

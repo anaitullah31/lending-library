54ed77e96055c55b066f2c27b4a7745a9351a407
anaitullah31 <anaitullah198@gmail.com>
2026-05-01 22:42:56 +0800

    User swiper slide on hero section

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,7 +1,28 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "../../app/style.css";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 const Hero = () => {
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty("--progress", 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };
   return (
     <section className="bg-[#ffffff] min-h-155 px-6 py-8 md:py-16">
       <div className="max-w-7xl mx-auto grid md:grid-cols-2 items-center gap-12">
@@ -41,13 +62,55 @@ const Hero = () => {
         {/* Right Image */}
         <div>
           <div className="relative w-full h-105 md:h-130 ">
-            <Image
-              src="/book1.jpg"
-              alt="Book on wooden shelf"
-              fill
-              priority
-              className="object-cover rounded-md"
-            />
+            <Swiper
+              spaceBetween={30}
+              centeredSlides={true}
+              autoplay={{
+                delay: 5000,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Autoplay, Pagination, Navigation]}
+              onAutoplayTimeLeft={onAutoplayTimeLeft}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <Image
+                  src="/book1.jpg"
+                  alt="Book on wooden shelf"
+                  fill
+                  priority
+                  className="object-cover rounded-md"
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image
+                  src="/book2.jpg"
+                  alt="Book on wooden shelf"
+                  fill
+                  priority
+                  className="object-cover rounded-md"
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image
+                  src="/book3.jpg"
+                  alt="Book on wooden shelf"
+                  fill
+                  priority
+                  className="object-cover rounded-md"
+                />
+              </SwiperSlide>
+              <div className="autoplay-progress" slot="container-end">
+                <svg viewBox="0 0 48 48" ref={progressCircle}>
+                  <circle cx="24" cy="24" r="20"></circle>
+                </svg>
+                <span ref={progressContent}></span>
+              </div>
+            </Swiper>
           </div>
 
           <p className="mt-8 text-[#363431] italic text-sm font-semibold leading-6">

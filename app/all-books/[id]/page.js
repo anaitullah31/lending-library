38ed4e20ebab60de62d293135493f7b21e75de13
anaitullah31 <anaitullah198@gmail.com>
2026-05-02 18:23:59 +0800

    Add suspense on book detials page

--- app/all-books/[id]/page.js
+++ app/all-books/[id]/page.js
@@ -1,12 +1,19 @@
 import BookDetailsCard from "@/app/components/BookDetailsCard";
 import { loadAllBooks } from "@/lib/utils";
+import { Suspense } from "react";
 
 const BookDetailsPage = async ({ params }) => {
   const { id } = await params;
   const books = await loadAllBooks();
   const singleBook = books.find((book) => book.id === parseInt(id));
 
-  return <BookDetailsCard book={singleBook} />;
+  return (
+    <>
+      <Suspense fallback={<p>Loading book details</p>}>
+        <BookDetailsCard book={singleBook} />
+      </Suspense>
+    </>
+  );
 };
 
 export default BookDetailsPage;
